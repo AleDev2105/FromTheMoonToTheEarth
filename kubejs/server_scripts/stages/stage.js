f94880644bafcf3345d2f895e8d95b411782ac86
Kessy2105 <ale.casale08@gmail.com>
2023-10-28 20:04:13 +0200

    Added items to stage system

--- kubejs/server_scripts/stages/stage.js
+++ kubejs/server_scripts/stages/stage.js
@@ -1,23 +1,19 @@
 const itemToStage = {
     // stage_name: [item_ids]
-
-    'moon_base': ['ad_astra:astrodux', 'minecraft:soul_soil', 'ad_astra:moon_cobblestone', 'ad_astra:moon_sand', 'ad_astra:moon_stone']
+    'moon_base': ['ad_astra:astrodux', 'minecraft:soul_soil', 'ad_astra:moon_cobblestone', 'ad_astra:moon_sand', 'ad_astra:moon_stone'],
+    'moon_lunar_tower': ['ad_astra:moon_stone_stairs', 'ad_astra:moon_stone_slab', 'ad_astra:polished_moon_stone', 'ad_astra:polished_moon_stone_stairs', 'ad_astra:polished_moon_stone_slab', 'ad_astra:moon_stone_brick_wall', 'ad_astra:moon_cobblestone_stairs', 'ad_astra:moon_cobblestone_slab', 'ad_astra:iron_pillar', 'ad_astra:desh_plating', 'ad_astra:desh_pillar', 'ad_astra:desh_plating_stairs', 'ad_astra:desh_plating_slab', 'ad_astra:moon_stone_bricks', 'ad_astra:moon_stone_brick_slab', 'ad_astra:moon_stone_brick_stairs', 'ad_astra:cracked_moon_stone_bricks', 'ad_astra:chiseled_moon_stone_bricks', 'ad_astra:chiseled_moon_stone_stairs', 'ad_astra:chiseled_moon_stone_slab'],
+    'moon_lunarian_village': ['ad_astra:moon_stone_brick_wall', 'ad_astra:polished_moon_stone_slab', 'ad_astra:polished_moon_stone_stairs', 'ad_astra:polished_moon_stone', 'ad_astra:iron_plating_pressure_plate', 'ad_astra:iron_plating_button', 'ad_astra:iron_plating_slab', 'ad_astra:iron_plating_stairs', 'ad_astra:iron_plating', 'ad_astra:chiseled_moon_stone_slab', 'ad_astra:chiseled_moon_stone_bricks', 'ad_astra:cracked_moon_stone_bricks', 'ad_astra:moon_cobblestone_slab', 'ad_astra:moon_cobblestone_stairs', 'ad_astra:desh_pillar', 'ad_astra:desh_plating_slab', 'ad_astra:desh_plating_stairs', 'ad_astra:desh_plating', 'ad_astra:moon_stone_brick_stairs', 'ad_astra:moon_stone_brick_slab', 'ad_astra:moon_stone_bricks', 'ad_astra:moon_stone_slab', 'ad_astra:moon_stone_stairs', 'minecraft:ladder', 'ad_astra:marked_iron_pillar', 'ad_astra:glowing_iron_pillar', 'ad_astra:iron_pillar'],
+    'moon_rover': ['ad_astra:tier_1_rover'],
+    'moon_wood': ['ad_astra:strophar_chest', 'ad_astra:strophar_ladder', 'ad_astra:strophar_fence_gate', 'ad_astra:strophar_fence', 'ad_astra:strophar_slab', 'ad_astra:strophar_stairs', 'ad_astra:strophar_planks', 'ad_astra:strophar_stem', 'ad_astra:strophar_cap', 'ad_astra:strophar_trapdoor', 'ad_astra:strophar_door']
 }
 
 ServerEvents.loaded(_ => {
-
     for (const stage in itemToStage) {
         if (itemToStage.hasOwnProperty.call(itemToStage, stage)) {
-            const item = itemToStage[stage];
-            addItemStage(item, stage)
+            let item = itemToStage[stage];
+            //items.forEach(item => {
+                addItemStage(item, stage)
+            //})
         }
     }
-
-    /*
-    itemToStage.forEach(stage, items => {
-        items.forEach(item => {
-            addItemStage(stage, item)
-        })
-    })
-    */
 })
